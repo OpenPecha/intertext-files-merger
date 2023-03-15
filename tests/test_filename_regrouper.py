95ef3081fd6eab39d1fbfb8cde047f2cac6f479b
tenzin-chime <tenchime27@gmail.com>
2023-03-15 12:38:09 +0530

    update test_filename

--- tests/test_filename_regrouper.py
+++ tests/test_filename_regrouper.py
@@ -7,16 +7,16 @@ def test_regroup_filename():
     expected_output = {
         "t001":{
             'bo': [
-            Path("./tests/data/t001-input/t001-01-padma.bo.xml"),
-            Path("./tests/data/t001-input/t001-03-jc.bo.xml")
+            Path("./tests/data/t001-input/t001-03-jc.bo.xml"),
+            Path("./tests/data/t001-input/t001-01-padma.bo.xml")
             ],
             'cn': [
             Path("./tests/data/t001-input/t001-01-padma.cn.xml"),
             Path("./tests/data/t001-input/t001-03-jc.cn.xml")
             ],
             'bo.cn': [
-            Path("./tests/data/t001-input/t001-01-padma.bo.cn.xml"),
-            Path("./tests/data/t001-input/t001-03-jc.bo.cn.xml")
+            Path("./tests/data/t001-input/t001-03-jc.bo.cn.xml"),
+            Path("./tests/data/t001-input/t001-01-padma.bo.cn.xml")
             ]
         }   
     }
