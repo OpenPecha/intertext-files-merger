90a135fd5c372650e03e74e69a2c2be771630a0a
tenzin-chime <tenchime27@gmail.com>
2023-04-03 15:04:10 +0530

    hot fix

--- src/intertext_files_merger/extract_msg.py
+++ src/intertext_files_merger/extract_msg.py
@@ -2,7 +2,7 @@ import re
 
 def get_file_names(commit_msg):
     if "merge" in commit_msg.lower():
-        input_filenames = re.findall(r'\[([^\]]+)\]', commit_msg)[0].split(', ')
+        input_filenames = re.findall(r'\[([^\]]+)\]', commit_msg)[0].split(',')
         return input_filenames
     else:
         print("Trigger word 'merge' not found in commit message.")
