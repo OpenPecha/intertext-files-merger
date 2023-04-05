c817fe781175219e1afd9cee98144c26fa85a626
tenzin-chime <tenchime27@gmail.com>
2023-04-05 12:08:01 +0530

    updated data name

--- tests/test_alignment_merger.py
+++ tests/test_alignment_merger.py
@@ -3,7 +3,7 @@ from intertext_files_merger.alignment_merger import get_alignment_text
 
 
 def test_merge_text():
-    expected_output=Path('./tests/data/t001-output/t001.bo.cn.xml').read_text(encoding="utf-8")
-    file_paths=[Path('./tests/data/t001-input/t001-01-padma.bo.cn.xml'),Path('./tests/data/t001-input/t001-03-jc.bo.cn.xml')]
+    expected_output=Path('./tests/data/t001-output/dmk-t341-t0202.bo.cn.xml').read_text(encoding="utf-8")
+    file_paths=[Path('./tests/data/t001-input/dmk-t341-t0202-01-padma-20221217.bo.cn.xml'),Path('./tests/data/t001-input/dmk-t341-t0202-03-JC-20221217.bo.cn.xml')]
     merged_alignment=get_alignment_text(file_paths)
     assert merged_alignment == expected_output
--- tests/test_text_merger.py
+++ tests/test_text_merger.py
@@ -3,13 +3,13 @@ from intertext_files_merger.text_merger import get_language_text
 
 
 def test_merge_text():
-    expected_output=Path('./tests/data/t001-output/t001.bo.xml').read_text(encoding="utf-8")
-    file_paths=['./tests/data/t001-input/t001-01-padma.bo.xml','./tests/data/t001-input/t001-03-jc.bo.xml']
+    expected_output=Path('./tests/data/t001-output/dmk-t341-t0202.bo.xml').read_text(encoding="utf-8")
+    file_paths=['./tests/data/t001-input/dmk-t341-t0202-01-padma-20221217.bo.xml','./tests/data/t001-input/dmk-t341-t0202-03-JC-20221217.bo.xml']
     merged_text=get_language_text(file_paths)
     assert merged_text == expected_output
 
-    expected_output_cn=Path('./tests/data/t001-output/t001.cn.xml').read_text(encoding="utf-8")
-    file_paths_cn=['./tests/data/t001-input/t001-01-padma.cn.xml','./tests/data/t001-input/t001-03-jc.cn.xml']
+    expected_output_cn=Path('./tests/data/t001-output/dmk-t341-t0202.cn.xml').read_text(encoding="utf-8")
+    file_paths_cn=['./tests/data/t001-input/dmk-t341-t0202-01-padma-20221217.cn.xml','./tests/data/t001-input/dmk-t341-t0202-03-JC-20221217.cn.xml']
     merged_text_cn=get_language_text(file_paths_cn)
     assert merged_text_cn == expected_output_cn
 
